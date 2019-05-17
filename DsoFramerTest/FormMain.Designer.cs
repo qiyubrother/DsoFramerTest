635d05deb13562aba39e45008f01b04318c46b63
zhenhua Liu <qiyubrother@outlook.com>
2019-05-17 18:40:32 +0800

    添加项目文件。

---
+++ DsoFramerTest/FormMain.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace DsoFramerTest
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.axFramerControl1 = new AxDSOFramer.AxFramerControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.axFramerControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axFramerControl1
+            // 
+            this.axFramerControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axFramerControl1.Enabled = true;
+            this.axFramerControl1.Location = new System.Drawing.Point(12, 41);
+            this.axFramerControl1.Name = "axFramerControl1";
+            this.axFramerControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axFramerControl1.OcxState")));
+            this.axFramerControl1.Size = new System.Drawing.Size(776, 397);
+            this.axFramerControl1.TabIndex = 3;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.axFramerControl1);
+            this.Controls.Add(this.button1);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axFramerControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private AxDSOFramer.AxFramerControl axFramerControl1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DsoFramerTest/FormMain.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using Microsoft.Win32;
+
+namespace DsoFramerTest
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileFullPath = $@"{System.Environment.CurrentDirectory}\dsoframer.ocx";
+            if (!File.Exists(fileFullPath))
+            {
+                // 容器控件丢失
+                return;
+            }
+
+            bool isRegisted = IsRegistered("00460182-9E5E-11D5-B7C8-B8269041DD57");
+            if (!isRegisted)
+            {
+                var op = new OpenFileDialog();
+                op.ShowDialog();
+                string strFileName = op.FileName;
+                //If the user does not cancel, open the document.
+                if (strFileName.Length != 0)
+                {
+                    this.axFramerControl1.Open(strFileName);
+                }
+                return;
+            }
+            else
+            {
+                axFramerControl1.Toolbars = false;
+                axFramerControl1.Menubar = false;
+                axFramerControl1.BorderStyle = DSOFramer.dsoBorderStyle.dsoBorderNone;
+
+                File.Copy(fileFullPath, $@"{System.Environment.SystemDirectory}\dsoframer.ocx", true);
+
+                Regist($@"{System.Environment.SystemDirectory}\dsoframer.ocx");
+            }
+        }
+
+        //注册dsoframer.ocx
+        private static bool Regist(string fileFullName)
+        {
+            bool result = false;
+         //   System.Diagnostics.Process p = System.Diagnostics.Process.Start("regsvr32", fileFullName + " /s");//注册完毕不显示是否成功的提示
+
+             System.Diagnostics.Process p = System.Diagnostics.Process.Start("regsvr32", fileFullName);//注册完毕显示是否成功的提示
+            if (p != null && p.HasExited)
+            {
+                Int32 exitCode = p.ExitCode;
+                if (exitCode == 0)
+                    result = true;
+            }
+            return result;
+        }
+        //获取当前操作系统的位数
+        private static string GetOSBitCount()
+        {
+            ConnectionOptions oConn = new ConnectionOptions();
+            System.Management.ManagementScope oMs = new System.Management.ManagementScope("\\\\localhost", oConn);
+            System.Management.ObjectQuery oQuery = new System.Management.ObjectQuery("select AddressWidth from Win32_Processor");
+            ManagementObjectSearcher oSearcher = new ManagementObjectSearcher(oMs, oQuery);
+            ManagementObjectCollection oReturnCollection = oSearcher.Get();
+            string addressWidth = null;
+
+            foreach (ManagementObject oReturn in oReturnCollection)
+                addressWidth = oReturn["AddressWidth"].ToString();
+
+            return addressWidth;
+        }
+        //判断控件是否已经注册
+        private static bool IsRegistered(String CLSID)
+        {
+            if (String.IsNullOrEmpty(CLSID))
+                return false;
+
+            String key = String.Format(@"CLSID\{{{0}}}", CLSID);
+            RegistryKey regKey = Registry.ClassesRoot.OpenSubKey(key);
+            if (regKey != null)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
