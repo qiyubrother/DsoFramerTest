3b55f2db2e4f6f94e8991d8646256eb012254466
zhenhua Liu <qiyubrother@outlook.com>
2019-08-09 13:23:47 +0800

    .

--- DsoFramerTest/FormMain.Designer.cs
+++ DsoFramerTest/FormMain.Designer.cs
@@ -30,87 +30,21 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
-            this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.btnPlay = new System.Windows.Forms.Button();
-            this.btnNext = new System.Windows.Forms.Button();
-            this.btnPrevious = new System.Windows.Forms.Button();
-            this.btnInnerPlay = new System.Windows.Forms.Button();
-            this.txtPID = new System.Windows.Forms.TextBox();
             this.pnlMain = new System.Windows.Forms.Panel();
             this.axFramerControl1 = new AxDSOFramer.AxFramerControl();
-            this.button2 = new System.Windows.Forms.Button();
             this.pnlMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axFramerControl1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Open file";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnOpen_Click);
-            // 
-            // btnPlay
-            // 
-            this.btnPlay.Location = new System.Drawing.Point(94, 13);
-            this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(75, 23);
-            this.btnPlay.TabIndex = 4;
-            this.btnPlay.Text = "play";
-            this.btnPlay.UseVisualStyleBackColor = true;
-            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
-            // 
-            // btnNext
-            // 
-            this.btnNext.Location = new System.Drawing.Point(256, 13);
-            this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(75, 23);
-            this.btnNext.TabIndex = 5;
-            this.btnNext.Text = "Next";
-            this.btnNext.UseVisualStyleBackColor = true;
-            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
-            // 
-            // btnPrevious
-            // 
-            this.btnPrevious.Location = new System.Drawing.Point(175, 12);
-            this.btnPrevious.Name = "btnPrevious";
-            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
-            this.btnPrevious.TabIndex = 6;
-            this.btnPrevious.Text = "Previous";
-            this.btnPrevious.UseVisualStyleBackColor = true;
-            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
-            // 
-            // btnInnerPlay
-            // 
-            this.btnInnerPlay.Location = new System.Drawing.Point(490, 13);
-            this.btnInnerPlay.Name = "btnInnerPlay";
-            this.btnInnerPlay.Size = new System.Drawing.Size(75, 23);
-            this.btnInnerPlay.TabIndex = 7;
-            this.btnInnerPlay.Text = "Inner play";
-            this.btnInnerPlay.UseVisualStyleBackColor = true;
-            this.btnInnerPlay.Click += new System.EventHandler(this.btnInnerPlay_Click);
-            // 
-            // txtPID
-            // 
-            this.txtPID.Location = new System.Drawing.Point(419, 13);
-            this.txtPID.Name = "txtPID";
-            this.txtPID.Size = new System.Drawing.Size(65, 21);
-            this.txtPID.TabIndex = 8;
-            // 
             // pnlMain
             // 
-            this.pnlMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlMain.BackColor = System.Drawing.Color.Bisque;
             this.pnlMain.Controls.Add(this.axFramerControl1);
-            this.pnlMain.Location = new System.Drawing.Point(12, 59);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
             this.pnlMain.Name = "pnlMain";
-            this.pnlMain.Size = new System.Drawing.Size(779, 562);
+            this.pnlMain.Size = new System.Drawing.Size(803, 633);
             this.pnlMain.TabIndex = 10;
             // 
             // axFramerControl1
@@ -120,54 +54,28 @@
             this.axFramerControl1.Location = new System.Drawing.Point(0, 0);
             this.axFramerControl1.Name = "axFramerControl1";
             this.axFramerControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axFramerControl1.OcxState")));
-            this.axFramerControl1.Size = new System.Drawing.Size(779, 562);
+            this.axFramerControl1.Size = new System.Drawing.Size(803, 633);
             this.axFramerControl1.TabIndex = 5;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(572, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(803, 633);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.pnlMain);
-            this.Controls.Add(this.txtPID);
-            this.Controls.Add(this.btnInnerPlay);
-            this.Controls.Add(this.btnPrevious);
-            this.Controls.Add(this.btnNext);
-            this.Controls.Add(this.btnPlay);
-            this.Controls.Add(this.button1);
             this.KeyPreview = true;
             this.Name = "FormMain";
             this.Text = "打开Office文件测试程序";
             this.pnlMain.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.axFramerControl1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button btnPlay;
-        private System.Windows.Forms.Button btnNext;
-        private System.Windows.Forms.Button btnPrevious;
-        private System.Windows.Forms.Button btnInnerPlay;
-        private System.Windows.Forms.TextBox txtPID;
         private System.Windows.Forms.Panel pnlMain;
         private AxDSOFramer.AxFramerControl axFramerControl1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
