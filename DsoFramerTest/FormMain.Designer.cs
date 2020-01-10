af0c188ddb8a1bfc3f4a33c2e5ad0bdabf7b72d5
13240137763@163.com <qiyubrother>
2020-01-10 21:53:57 +0800

    .

--- DsoFramerTest/FormMain.Designer.cs
+++ DsoFramerTest/FormMain.Designer.cs
@@ -33,6 +33,9 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pnlMain = new System.Windows.Forms.Panel();
             this.axFramerControl1 = new AxDSOFramer.AxFramerControl();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.pnlMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axFramerControl1)).BeginInit();
             this.SuspendLayout();
@@ -40,6 +43,8 @@
             // pnlMain
             // 
             this.pnlMain.BackColor = System.Drawing.Color.Bisque;
+            this.pnlMain.Controls.Add(this.label1);
+            this.pnlMain.Controls.Add(this.button1);
             this.pnlMain.Controls.Add(this.axFramerControl1);
             this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlMain.Location = new System.Drawing.Point(0, 0);
@@ -49,13 +54,35 @@
             // 
             // axFramerControl1
             // 
-            this.axFramerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.axFramerControl1.Enabled = true;
-            this.axFramerControl1.Location = new System.Drawing.Point(0, 0);
+            this.axFramerControl1.Location = new System.Drawing.Point(0, 47);
             this.axFramerControl1.Name = "axFramerControl1";
             this.axFramerControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axFramerControl1.OcxState")));
-            this.axFramerControl1.Size = new System.Drawing.Size(803, 633);
+            this.axFramerControl1.Size = new System.Drawing.Size(803, 586);
             this.axFramerControl1.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "x86";
             // 
             // FormMain
             // 
@@ -68,6 +95,7 @@
             this.Name = "FormMain";
             this.Text = "dsoframer.ocx 测试程序";
             this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axFramerControl1)).EndInit();
             this.ResumeLayout(false);
 
@@ -77,6 +105,9 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Panel pnlMain;
         private AxDSOFramer.AxFramerControl axFramerControl1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- DsoFramerTest/FormMain.cs
+++ DsoFramerTest/FormMain.cs
@@ -25,5 +25,36 @@ namespace DsoFramerTest
         {
             base.OnShown(e);
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (openFileDialog1.FileName.EndsWith(".xlsx", StringComparison.InvariantCultureIgnoreCase)
+                        || openFileDialog1.FileName.EndsWith(".xls", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        this.axFramerControl1.Open(openFileDialog1.FileName, true, "Excel.Sheet", null, null);
+                    }
+                    else if (openFileDialog1.FileName.EndsWith(".pptx", StringComparison.InvariantCultureIgnoreCase)
+                        || openFileDialog1.FileName.EndsWith(".ppt", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        this.axFramerControl1.Open(openFileDialog1.FileName, true, "PowerPoint.Show", null, null);
+                    }
+                    else if (openFileDialog1.FileName.EndsWith(".docx", StringComparison.InvariantCultureIgnoreCase)
+                        || openFileDialog1.FileName.EndsWith(".doc", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        this.axFramerControl1.Open(openFileDialog1.FileName, true, "Word.Document", null, null);
+                    }
+                }
+                catch(System.Runtime.InteropServices.COMException come)
+                {
+                    MessageBox.Show(come.Message);
+                }
+
+            }
+            
+        }
     }
 }
